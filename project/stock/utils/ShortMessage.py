bbf29b89eeda615d0e82b5b704bf9f2f6003bff6
chenjiankum <kkltcjk@gmail.com>
2016-04-05 16:48:13 +0800

    none

--- project/stock/utils/ShortMessage.py
+++ project/stock/utils/ShortMessage.py
@@ -1,15 +1,18 @@
 # -*- coding: utf-8 -*-
 import urllib
 import urllib2
-import cookielib
 import sys
-from stock.utils.CParser  import CParser
+import time
+from stock.utils.CParser import CParser
 
 
 SERVICE_URL = 'http://106.ihuyi.cn/webservice/sms.php?method=Submit'
 
 reload(sys)
 sys.setdefaultencoding('utf-8')
+
+send_flag = True
+
 
 class ShortMessage:
     def __init__(self, message, phone):
@@ -19,6 +22,7 @@ class ShortMessage:
         self.message = message
 
     def send(self):
+        global send_flag
         data = {
             'account': self.user,
             'password': self.password,
@@ -27,11 +31,18 @@ class ShortMessage:
         }
 
         try:
-            encoding_data = urllib.urlencode(data)
-            req = urllib2.Request(url = SERVICE_URL, data = encoding_data)
-            res = urllib2.urlopen(req)
-            result = res.read()
+            while send_flag is False:
+                time.sleep(1)
+            send_flag = False
 
+            encoding_data = urllib.urlencode(data)
+            req = urllib2.Request(url=SERVICE_URL, data=encoding_data)
+            res = urllib2.urlopen(req)
+
+            time.sleep(1)
+            send_flag = True
+
+            result = res.read()
             parser = CParser()
             parser.feed(result)
 
@@ -40,4 +51,3 @@ class ShortMessage:
             return dict
         except Exception as e:
             print e
-
